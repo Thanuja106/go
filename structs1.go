0bac1fe22e4e7f0203b3ce96b92bb10d263af4b0
Thanuja106 <54537463+Thanuja106@users.noreply.github.com>
2019-09-16 12:06:02 +0530

    Create structs1.go

---
+++ structs1.go
@@ -0,0 +1,20 @@
+/*type Circle struct{
+  radius float32
+}
+type Rectangle struct{
+  length float32
+  breadth float32
+}
+func (c *Circle) Area(r float32) float32{
+  c.radius = 4
+  return 22*r*r/7
+}
+func (r *Rectangle) Area(l, b float32) float32{
+  return l*b
+}
+func main(){
+ c := Circle{}
+ d := Rectangle{}
+ fmt.Println(c.Area(3.5), c.radius)
+ fmt.Println(d.Area(3.5, 5.5))
+}*/
