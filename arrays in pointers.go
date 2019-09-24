3147c72d227b97e4cbfe04f787c229b870a8f3ca
Thanuja106 <54537463+Thanuja106@users.noreply.github.com>
2019-09-24 15:40:08 +0530

    Update arrays in pointers.go

--- arrays in pointers.go
+++ arrays in pointers.go
@@ -14,3 +14,25 @@ func change(p *[3]int){
     (*p)[2]=4||p[2]=4 are the same                                                     output:[2 3 4]
 
 }
+
+/--------------------------------------------------------------------------------------------------------------------
+package main
+
+import "fmt"
+
+func main() {
+a:=[6]int{1,2,3,4,5,6}
+var p [4]*int
+for i:=0;i<len(p);i++{
+  p[i]=&a[i]
+}
+for i:=0;i<len(p);i++{
+  fmt.Println(*p[i],"\n")
+}
+}
+
+OUTPUT:
+1
+2
+3
+4
