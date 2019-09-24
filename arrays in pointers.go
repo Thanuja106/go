c9f57d776fbcf66604ccbf937e3dabc9eec0743b
Thanuja106 <54537463+Thanuja106@users.noreply.github.com>
2019-09-24 10:59:00 +0530

    Update arrays in pointers.go

--- arrays in pointers.go
+++ arrays in pointers.go
@@ -8,6 +8,7 @@ change(&a)
 fmt.Println(a)
 }
 func change(p *[3]int){
+  //*p[0]!=(*p)[0]
   (*p)[0]=2
    (*p)[1]=3
     (*p)[2]=4                                                     output:[2 3 4]
