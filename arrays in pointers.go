6155848e0933ef1c5dfa632584b98009089a9867
Thanuja106 <54537463+Thanuja106@users.noreply.github.com>
2019-09-24 10:57:18 +0530

    Create arrays in pointers.go

---
+++ arrays in pointers.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+a:=[3]int{1,2,3}
+change(&a)
+fmt.Println(a)
+}
+func change(p *[3]int){
+  (*p)[0]=2
+   (*p)[1]=3
+    (*p)[2]=4                                                     output:[2 3 4]
+
+}
